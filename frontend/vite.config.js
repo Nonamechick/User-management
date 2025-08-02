9eda20c5ce9018ba7cf132d005e5c55d26a5757e
Khasan Otazhonov <otajonovxasan350@gmail.com>
2025-08-02 19:21:17 +0500

    package updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://user-management-1-ystr.onrender.com',
         changeOrigin: true,
         secure: false,
       },
